d937f2cd14d3df9a07152ebd4c3132c422056c01
Vitalii Vitkovskyi <filosoff13@gmail.com>
2021-08-19 16:38:46 +0300

    [PHP-Basics] Correct validation

--- src/basics/Basics.php
+++ src/basics/Basics.php
@@ -5,8 +5,16 @@ namespace basics;
 class Basics implements BasicsInterface
 {
 
+    protected $validate;
+
+    public function __construct()
+    {
+        $this->validate = new BasicsValidator;
+    }
+
     public function getMinuteQuarter(int $minute): string
     {
+        $this->validate->isMinutesException($minute);
         if ($minute > 0 && $minute < 16){
             return "first";
         } elseif ($minute > 15 && $minute < 31){
@@ -15,38 +23,30 @@ class Basics implements BasicsInterface
             return "third";
         } elseif (($minute > 45 && $minute <= 60) || $minute === 0){
             return "fourth";
-        } else {
-            (new BasicsValidator)->isMinutesException($minute);
         }
     }
 
     public function isLeapYear(int $year): bool
     {
-        if ($year < 1900){
-            (new BasicsValidator)->isYearException($year);
-        }
+        $this->validate->isYearException($year);
         return ($year % 4 === 0 && $year % 100 !== 0) || $year % 400 === 0;
     }
 
     public function isSumEqual(string $input): bool
     {
-        $input_len = strlen($input);
-        if ($input_len !== 6){
-            (new BasicsValidator)->isValidStringException($input);
-        } else {
-            $arr = str_split($input);
-            $sum1 = 0;
-            $sum2 = 0;
+        $this->validate->isValidStringException($input);
 
-            for ($i = 0; $i < 6 ; $i++)
-            {
-                if ($i < 3){
-                    $sum1 += $arr[$i];
-                } else{
-                    $sum2 += $arr[$i];
-                }
+        $arr = str_split($input);
+        $sum1 = 0;
+        $sum2 = 0;
+        for ($i = 0; $i < 6 ; $i++)
+        {
+            if ($i < 3){
+                $sum1 += $arr[$i];
+            } else{
+                $sum2 += $arr[$i];
             }
-            return ($sum1 === $sum2);
         }
+        return ($sum1 === $sum2);
     }
 }
--- src/basics/BasicsValidator.php
+++ src/basics/BasicsValidator.php
@@ -7,16 +7,23 @@ class BasicsValidator implements BasicsValidatorInterface
 
     public function isMinutesException(int $minute): void
     {
-        throw new \InvalidArgumentException("invalid argument is:" . $minute);
+        if ($minute < 0 || $minute > 60) {
+            throw new \InvalidArgumentException("invalid argument is:" . $minute);
+        }
     }
 
     public function isYearException(int $year): void
     {
-        throw new \InvalidArgumentException("input year is" . $year . "and it`s lower than 1900");
+        if ($year < 1900){
+            throw new \InvalidArgumentException("input year is" . $year . "and it`s lower than 1900");
+        }
     }
 
     public function isValidStringException(string $input): void
     {
-        throw new \InvalidArgumentException("Input contains more then 6 digits");
+        $input_len = strlen($input);
+        if ($input_len !== 6){
+            throw new \InvalidArgumentException("Input contains more then 6 digits");
+        }
     }
 }
